ae571aafe3293a790b61670903e626bc7fe8c288
Ceri Perkins <ceri_perkins@hotmail.com>
2025-03-16 20:08:05 +0000

    add createSum function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,48 @@
 $(document).ready(function() {
+
+});
     
-// button functions are below 
+/**
+ * This Functoni is called upon to generate a random sum 
+ * that is used for both games on the page
+ */
+function createSum() {
 
-    $(".rules").click(function(){
-        $("#rules-box").toggle();
-    })
+    // Creates two random numbers between 1 and 20
+    let num1 = Math.floor(Math.random() * 20) + 1;
+    let num2 = Math.floor(Math.random() * 20) + 1;
+        
+    // Chooses a random operator
+    let operators = ['+', '-', '*', '/'];
+    let operator = operators[Math.floor(Math.random() * operators.length)];
 
+    //combine random numbers with the randomly chosen operator
+
+    let finalSum = `${num1} ${operator} ${num2}`;
+        
+    // Calculate the answer
+
+    let answer;
+    
+      if (operator === "+") {
+          answer = num1 + num2;
+      } else if (operator === "-") {
+          answer = num1 - num2;
+      } else if (operator === "*" ) {
+          answer = num1 * num2;
+      }  else if (operator === "/"){
+	      answer = num1 / num2;
+      }   else {
+          alert(`Unknown Operator: ${operator}`);
+          throw `Unknown Operator: ${operator}. Oh the humanity! Aborting!`;
+      }
+    
+        return { question, answer };
+}
+   
+
+    // Revealing button functions are below 
+   
     $("#game-a-button").click(function(){
         if($("#game-a-box").hasClass("hidden")){
         $("#game-a-box").removeClass("hidden");
@@ -30,7 +67,12 @@ $(document).ready(function() {
         $("#combined-scores-box").css({display: flex});
     })
 
+    $(".rules").click(function(){
+        $("#rules-box").toggle();
+    })
+
+    
 
 
 
-});
+
